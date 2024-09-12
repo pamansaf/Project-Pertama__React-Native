8bc20d8905544ef163bc028d6099e21f208fb92a
pamansaf <safwan.ssy@gmail.com>
2024-09-12 16:07:21 +0700

    Text component & styling with tailwindcss

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import {View, Text} from 'react-native';
+const App = () => {
+  return (
+    <View className="w-full min-h-screen flex justify-center items-center bg-teal-100 ">
+      <Text className="text-3xl font-bold text-blue-600">Welcome Back</Text>
+      <Text className="font-semibold text-[#e94235]">
+        Project React_Native Pertama
+      </Text>
+    </View>
+  );
+};
+
+export default App;
