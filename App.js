70a1c4adb66966e4e852919fe73118c903351ac7
pamansaf <safwan.ssy@gmail.com>
2024-09-17 15:24:29 +0700

    FlatList Array of Object - Stayling

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, FlatList, StatusBar, Image} from 'react-native';
 
-const negara = [
-  'Indonesia',
-  'Jepang',
-  'Korea',
-  'Malaysia',
-  'Australia',
-  'Cina',
-];
+const negara = ['Indonesia', 'Jepang', 'Korea', 'Malaysia', 'Australia'];
 
 const daftarNegara = [
   {
-    nama: 'indonesia',
+    nama: 'Indonesia',
     deskripsi: 'ini adalah nama negara',
     bendera:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Flag_of_Indonesia_%28physical_version%29.svg/640px-Flag_of_Indonesia_%28physical_version%29.svg.png',
@@ -33,7 +26,7 @@ const App = () => {
         data={negara}
         renderItem={({item}) => (
           <View className="mx-5 my-1">
-            <Text className="bg-[#1f1f1f] text-white text-center py-3 rounded-md">
+            <Text className="bg-[#1f1f1f] text-white text-center py-5 rounded-md">
               {item}
             </Text>
           </View>
@@ -43,10 +36,17 @@ const App = () => {
       <FlatList
         data={daftarNegara}
         renderItem={({item}) => (
-          <View className="flex flex-col justify-center items-center bg-[#1f1f1f] my-3 mx-5 py-5 shadow-xl">
-            <Image source={{uri: item.bendera}} className="w-[100] h-[70]" />
-            <Text className="text-white pt-3">{item.nama}</Text>
-            <Text className="text-white">{item.deskripsi}</Text>
+          <View className="flex flex-row items-center bg-[#1f1f1f] my-2 mx-5 py-8 px-5 shadow-xl">
+            <View className="flex-1">
+              <Text className="text-white font-semibold text-lg">
+                {item.nama}
+              </Text>
+              <Text className="text-white">{item.deskripsi}</Text>
+            </View>
+            <Image
+              source={{uri: item.bendera}}
+              className="w-[75] h-[50] rounded-sm"
+            />
           </View>
         )}
       />
